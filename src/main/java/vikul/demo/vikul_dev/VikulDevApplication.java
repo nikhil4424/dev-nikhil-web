fe4df94af53e1a2ce302065f21b75619189d52d9
vikul <you@example.com>
2024-05-23 21:19:12 +0530

    Dev application , added index.html

---
+++ src/main/java/vikul/demo/vikul_dev/VikulDevApplication.java
@@ -0,0 +1,13 @@
+package vikul.demo.vikul_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VikulDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VikulDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vikul/demo/vikul_dev/VikulDevApplicationTests.java
@@ -0,0 +1,13 @@
+package vikul.demo.vikul_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VikulDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
